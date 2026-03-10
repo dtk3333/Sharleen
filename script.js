da58bb07f5d775b012979f86420383d776e7b48a
dtk3333 <goalgetterrr333@gmail.com>
2026-03-10 20:31:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+function unlock(){
+
+let pass=document.getElementById("passcode").value;
+
+if(pass==="laska"){
+
+document.getElementById("lockScreen").style.display="none";
+document.getElementById("museum").classList.remove("hidden");
+
+startEffects();
+
+}
+else{
+document.getElementById("error").innerText="Wrong password";
+}
+
+}
+
+
+function startEffects(){
+
+let sparkleContainer=document.getElementById("sparkles");
+
+for(let i=0;i<60;i++){
+
+let s=document.createElement("span");
+
+s.style.left=Math.random()*100+"vw";
+s.style.top=Math.random()*100+"vh";
+
+sparkleContainer.appendChild(s);
+
+}
+
+let butterflyContainer=document.getElementById("butterflies");
+
+for(let i=0;i<5;i++){
+
+let b=document.createElement("img");
+
+b.src="butterfly.png";
+
+b.classList.add("butterfly");
+
+b.style.left=Math.random()*100+"vw";
+
+butterflyContainer.appendChild(b);
+
+}
+
+}
